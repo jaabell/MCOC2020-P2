a4bb8f339917a1cb32c9c8dabfee0e1d6276f104
IgnacioInostroza <69361237+IgnacioInostroza@users.noreply.github.com>
2020-09-28 22:50:33 -0300

    Update barra.py

--- barra.py
+++ barra.py
@@ -5,41 +5,36 @@ g = 9.81 #kg*m/s^2
 
 class Barra(object):
 
-	"""Constructor para una barra"""
-	def __init__(self, ni, nj, R, t, E, ρ, σy):
-		super(Barra, self).__init__()
-		self.ni = ni
-		self.nj = nj
-		self.R = R
-		self.t = t
-		self.E = E
-		self.ρ = ρ
-		self.σy = σy
+    """Constructor para una barra.
+    Los datos en orden son: nodo_i, nodo_j, radio, espesor, Modulo E, densidad, fluencia"""
+    def __init__(self, ni, nj, R, t, E, ρ, σy):
+        super(Barra, self).__init__()
+        self.ni = ni
+        self.nj = nj
+        self.R = R
+        self.t = t
+        self.E = E
+        self.ρ = ρ
+        self.σy = σy
 
-	def obtener_conectividad(self):
-		"""Implementar"""
-		return 
+    def obtener_conectividad(self):
+        return [self.n_i,self.n_j]
 
-	def calcular_area(self):
-		"""Implementar"""
-		return 
+    def calcular_area(self):
+        """Calcula el area de una barra circular"""
+        return np.pi*(self.R**2-(self.R-self.t)**2)
 
-	def calcular_largo(self, reticulado):
-		"""Devuelve el largo de la barra. 
-		xi : Arreglo numpy de dimenson (3,) con coordenadas del nodo i
-		xj : Arreglo numpy de dimenson (3,) con coordenadas del nodo j
-		"""
-		"""Implementar"""
-		return 
+    def calcular_largo(self, barra):
+        i=barra.obtener_coordenada_nodal(self.ni)
+        j=barra.obtener_coordenada_nodal(self.nj)
+        z=i-j
+        return np.sqrt(np.dot(z,z))
 
-	def calcular_peso(self, reticulado):
-		"""Devuelve el largo de la barra. 
-		xi : Arreglo numpy de dimenson (3,) con coordenadas del nodo i
-		xj : Arreglo numpy de dimenson (3,) con coordenadas del nodo j
-		"""
-		"""Implementar"""
-		return 
-
+    def calcular_peso(self, barra):
+            """Entrega el peso de una barra """
+            L = self.calcular_largo(barra)
+            A = self.calcular_area()
+            return self.ρ * A * L * g
 
 
 
